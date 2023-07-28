50322c8851262e06365f37c086946378651fc337
Luca Serra <luca.serra@hotmail.fr>
2023-07-28 11:22:30 +0200

    enh: add main script to orchestrate resources creation

---
+++ bin/main.sh
@@ -0,0 +1,37 @@
+source config/.env.shared
+
+set -e
+
+# # Create service accounts
+gcloud iam service-accounts create ${SERVICE_ACCOUNT_ID_PIPELINE} \
+    --description=" SA used by a cloud function to run Vertex pipelines" \
+    --display-name="Vertex pipelines runner CF" \
+    --project=${PROJECT_ID}
+
+gcloud iam service-accounts create ${SERVICE_ACCOUNT_ID_SCHEDULER} \
+    --description="SA used by a cloud scheduler to trigger a cloud function" \
+    --display-name="Cloud scheduler SA" \
+    --project=${PROJECT_ID}
+
+# Create cloud resources
+bash bin/create_bucket.sh
+bash bin/create_artifact_registry.sh
+bash bin/deploy_cloud_function.sh
+bash bin/create_cloud_scheduler.sh
+
+# Grant permissions
+gcloud projects add-iam-policy-binding ${PROJECT_ID} \
+    --member="serviceAccount:${SERVICE_ACCOUNT_ID_PIPELINE}@${PROJECT_ID}.iam.gserviceaccount.com" \
+    --role="roles/aiplatform.user"
+
+gcloud storage buckets add-iam-policy-binding ${PIPELINE_ROOT_PATH} \
+    --member="serviceAccount:${SERVICE_ACCOUNT_ID_PIPELINE}@${PROJECT_ID}.iam.gserviceaccount.com" \
+    --role="roles/storage.objectAdmin"
+
+gcloud functions add-iam-policy-binding ${CLOUD_FUNCTION_NAME} --region=${REGION} \
+    --member="serviceAccount:${SERVICE_ACCOUNT_ID_SCHEDULER}@${PROJECT_ID}.iam.gserviceaccount.com" \
+    --role="roles/cloudfunctions.invoker"
+
+gcloud artifacts repositories add-iam-policy-binding ${REPOSITORY_NAME} --location=${REGION} \
+    --member="serviceAccount:${SERVICE_ACCOUNT_ID_PIPELINE}@${PROJECT_ID}.iam.gserviceaccount.com" \
+    --role="roles/artifactregistry.writer"
