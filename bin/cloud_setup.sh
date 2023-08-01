8ae3cff71a8fa93f9eb64c1cf184887406cd1dae
Luca Serra <luca.serra@hotmail.fr>
2023-08-01 11:30:45 +0200

    enh: parametrize cron schedule

--- bin/cloud_setup.sh
+++ bin/cloud_setup.sh
@@ -47,9 +47,9 @@ parent="projects/${PROJECT_ID}/locations/${REGION}"
 gcloud scheduler jobs create http schedule_pipeline \
     --uri https://${REGION}-${PROJECT_ID}.cloudfunctions.net/${CLOUD_FUNCTION_NAME} \
     --location ${REGION} \
-    --schedule "*/5 * * * *" \
+    --schedule ${CRON_SCHEDULE} \
     --oidc-service-account-email ${SERVICE_ACCOUNT_ID_SCHEDULER}@${PROJECT_ID}.iam.gserviceaccount.com \
-    --message-body-from-file config/cloud_scheduler_params.json
+    --message-body-from-file config/cloud_function_params.json
 
 # 3. Grant permissions: create bindings
 gcloud projects add-iam-policy-binding ${PROJECT_ID} \
