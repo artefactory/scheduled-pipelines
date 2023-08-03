da6c2c64682a91cdbefabdca2479735e350d1917
Luca Serra <luca.serra@hotmail.fr>
2023-08-03 17:13:02 +0200

    clean: rename conda env

--- bin/install.sh
+++ bin/install.sh
@@ -1,15 +1,15 @@
 #!/bin/bash -e
 
-read -p "Want to install conda env named 'test-pipelines'? (y/n)" answer
+read -p "Want to install conda env named 'scheduled-pipelines'? (y/n)" answer
 if [ "$answer" = "y" ]; then
   echo "Installing conda env..."
-  conda create -n test-pipelines python=3.10 -y
+  conda create -n scheduled-pipelines python=3.10 -y
   source $(conda info --base)/etc/profile.d/conda.sh
-  conda activate test-pipelines
+  conda activate scheduled-pipelines
   echo "Installing requirements..."
   pip install -r requirements-developer.txt
-  python3 -m ipykernel install --user --name=test-pipelines
-  conda install -c conda-forge --name test-pipelines notebook -y
+  python3 -m ipykernel install --user --name=scheduled-pipelines
+  conda install -c conda-forge --name scheduled-pipelines notebook -y
   conda install grpcio # See issue https://stackoverflow.com/a/73245207/13891969
   echo "Installing pre-commit..."
   make install_precommit
