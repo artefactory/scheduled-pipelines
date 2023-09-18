211294ffd7c435d91f0c0dc70020519463cd0d2b
Luca Serra <luca.serra@hotmail.fr>
2023-09-18 11:15:50 +0200

    enh: detect OS to apply sed

--- bin/create_tf_state_bucket.sh
+++ bin/create_tf_state_bucket.sh
@@ -8,5 +8,13 @@ gcloud storage buckets create gs://${project_id}-scheduled-pipelines-tf-state \
     --project=${project_id} \
     --location=${region}
 
-
-sed -i '' "s/{PLACEHOLDER_FOR_SED}/$project_id/g" terraform/terraform.tf # Syntax for MacOS, see https://github.com/danielebailo/couchdb-dump/issues/35#issuecomment-148106536
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    echo "Linux detected"
+    sed -i "s/{PLACEHOLDER_FOR_SED}/$project_id/g" terraform/terraform.tf # Syntax for Linux
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    echo "MacOS detected"
+    sed -i '' "s/{PLACEHOLDER_FOR_SED}/$project_id/g" terraform/terraform.tf # Syntax for MacOS, see https://github.com/danielebailo/couchdb-dump/issues/35#issuecomment-148106536
+else
+    echo "OS not supported. Replace {PLACEHOLDER_FOR_SED} in terraform/terraform.tf with ${project_id} manually."
+    exit 1
+fi
