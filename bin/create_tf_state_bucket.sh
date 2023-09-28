a2cc7622890b6c8c25d543c357201b2f7ef67c68
Luca Serra <luca.serra@hotmail.fr>
2023-09-28 10:32:05 +0200

    fix: add r flag for old yq versions

--- bin/create_tf_state_bucket.sh
+++ bin/create_tf_state_bucket.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
-region=$(yq -e .project.region scheduled_pipelines_config.yaml)
-project_id=$(yq -e .project.id scheduled_pipelines_config.yaml)
+region=$(yq -er .project.region scheduled_pipelines_config.yaml)
+project_id=$(yq -er .project.id scheduled_pipelines_config.yaml)
 bucket_name=${project_id}-scheduled-pipelines-tf-state
 
 bucket_exists=$(gsutil list -p ${project_id} | grep ${bucket_name} | wc -l)
